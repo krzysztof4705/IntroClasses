e78ac990f1cacf6d62a1a0c0e701b497743be2be
krzysztof4705 <kdobrogowski06@gmail.com>
2026-03-09 11:16:36 +0100

    Use Console.ReadKey

--- IntroClasses/Program.cs
+++ IntroClasses/Program.cs
@@ -1,28 +1,21 @@
 ﻿using IntroClasses;
 
-public class Program
+
+Player hero = new Player();
+hero.Display();
+ConsoleKeyInfo input = Console.ReadKey(true);
+switch (input.Key)
 {
-    public static void Main()
-    {
-        Player hero = new Player();
-        hero.Display();
-        // Console.ReadKey(true);
-        string input = Console.ReadLine()!;
-        switch (input)
-        {
-            case "s":
-                hero.Move(0, 1);
-                break;
-            case "w":
-                hero.Move(0, -1);
-                break;
-            case "a":
-                hero.Move(-1, 0);
-                break;
-            case "d":
-                hero.Move(1, 0);
-                break;
-        }
-        hero.Display();
-    }
-}
+    case ConsoleKey.S:
+        hero.Move(0, 1);
+        break;
+    case ConsoleKey.W:
+        hero.Move(0, -1);
+        break;
+    case ConsoleKey.A:
+        hero.Move(-1, 0);
+        break;
+    case ConsoleKey.D:
+        hero.Move(1, 0);
+        break;
+} hero.Display();
