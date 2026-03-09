3c35a44f3c4963dc8578ad0e3508a4b38cedc306
krzysztof4705 <kdobrogowski06@gmail.com>
2026-03-09 11:20:51 +0100

    added while

--- IntroClasses/Program.cs
+++ IntroClasses/Program.cs
@@ -3,19 +3,24 @@
 
 Player hero = new Player();
 hero.Display();
-ConsoleKeyInfo input = Console.ReadKey(true);
-switch (input.Key)
+while (true)
 {
-    case ConsoleKey.S:
-        hero.Move(0, 1);
-        break;
-    case ConsoleKey.W:
-        hero.Move(0, -1);
-        break;
-    case ConsoleKey.A:
-        hero.Move(-1, 0);
-        break;
-    case ConsoleKey.D:
-        hero.Move(1, 0);
-        break;
-} hero.Display();
+    ConsoleKeyInfo input = Console.ReadKey(true);
+    switch (input.Key)
+    {
+        case ConsoleKey.S:
+            hero.Move(0, 1);
+            break;
+        case ConsoleKey.W:
+            hero.Move(0, -1);
+            break;
+        case ConsoleKey.A:
+            hero.Move(-1, 0);
+            break;
+        case ConsoleKey.D:
+            hero.Move(1, 0);
+            break;
+    }
+
+    hero.Display();
+}
