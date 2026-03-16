4f9f889b41a2dffb0b339217069ab5709162cf30
krzysztof4705 <kdobrogowski06@gmail.com>
2026-03-16 11:41:27 +0100

    TakeTurn

--- IntroClasses/Player.cs
+++ IntroClasses/Player.cs
@@ -17,4 +17,33 @@ public class Player
         _x += diffX;
         _y += diffY;
     }
+   
+
+    public bool TakeTurn()
+    {
+        bool isPlaying = true;
+        ConsoleKeyInfo input = Console.ReadKey(true);
+        Console.SetCursorPosition(_x, _y);
+        Console.Write(" ");
+        switch (input.Key)
+        {
+            case ConsoleKey.S:
+                Move(0, 1);
+                break;
+            case ConsoleKey.W:
+                Move(0, -1);
+                break;
+            case ConsoleKey.A:
+                Move(-1, 0);
+                break;
+            case ConsoleKey.D:
+                Move(1, 0);
+                break;
+             case ConsoleKey.Q:
+                isPlaying = false;
+                  break;
+        }
+        Display();
+        return isPlaying;
+    }
 }
--- IntroClasses/Program.cs
+++ IntroClasses/Program.cs
@@ -5,26 +5,6 @@ Player hero = new Player();
 hero.Display();
 while (isPlaynig)
 {
-    ConsoleKeyInfo input = Console.ReadKey(true);
-    switch (input.Key)
-    {
-        case ConsoleKey.S:
-            hero.Move(0, 1);
-            break;
-        case ConsoleKey.W:
-            hero.Move(0, -1);
-            break;
-        case ConsoleKey.A:
-            hero.Move(-1, 0);
-            break;
-        case ConsoleKey.D:
-            hero.Move(1, 0);
-            break;
-        case ConsoleKey.Q:
-            isPlaynig = false;
-            break;
-    }
-
-    hero.Display();
+    hero.TakeTurn();
 }
 Console.WriteLine("Goodbye!");
